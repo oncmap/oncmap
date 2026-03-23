bfb0172b203ca613d9adbe305b121263141ad5e4
Michal Kouril <michal@acai.local>
2026-03-23 16:25:50 -0400

    Packager update.

--- build_pkg.sh
+++ build_pkg.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-docker run -it -v `pwd`/..:`pwd`/.. -w `pwd` rocker/shiny:4.3.3 R -e 'install.packages("devtools");devtools::install_deps();devtools::build(".")'
-
+docker run -it -v `pwd`/..:`pwd`/.. -w `pwd` rocker/r-ver:4.5.3 R -e 'system("apt update");system("apt install -y pandoc");install.packages(c("remotes","devtools","pak"));pak::local_install_deps();devtools::build(".")'
